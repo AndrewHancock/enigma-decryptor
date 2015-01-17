db85a9e2377f981d90c7ea95630c66cf5f14d9e1
Andrew Hancock <AndrewNeilHancock@gmail.com>
2015-01-16 21:15:45 -0500

    Formatted files.

--- src/enigma/Test.java
+++ src/enigma/Test.java
@@ -1,15 +1,24 @@
 package enigma;
 
-public class Test {
+public class Test
+{
 
-		public static void main(String[] args)
-		{			
-			EnigmaMachine machine = EnigmaFactory.buildEnigmaMachine();
-			
-			String test = "AAAAAAAAAAAAAAAAAAAAAAAAAAA";			
-			System.out.println(test);
-			
-			machine.setRotors(5,  9, 14);
-			System.out.println(machine.encodeLine(test));
-		}
+	public static void main(String[] args)
+	{
+		// Construct the machine
+		EnigmaMachine machine = EnigmaFactory.buildEnigmaMachine();
+
+		// Print the test string
+		String test = "AAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		System.out.println(test);
+
+		// Set the rotors, encrypt the string and print the results
+		machine.setRotors(5, 9, 14);
+		String encodedString = machine.encodeLine(test);
+		System.out.println(encodedString);
+
+		// Reset the rotors, decrypt the string and print the results
+		machine.setRotors(5, 9, 14);
+		System.out.println(machine.encodeLine(encodedString));
+	}
 }
